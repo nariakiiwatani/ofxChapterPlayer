142705d6e55ebe38a5100fbc9cbc7ce1ddaf2225
nariakiiwatani <nariakiiwatani@annolab.com>
2022-02-18 10:24:52 +0900

    TextureViewer extends ofBaseHasTexture

--- src/sources/TextureViewer.h
+++ src/sources/TextureViewer.h
@@ -4,16 +4,20 @@
 #include "ofTexture.h"
 #include "ofImage.h"
 
-class TextureViewer : public ofx::chapterplayer::PlayerBase
+class TextureViewer : public ofx::chapterplayer::PlayerBase, public ofBaseHasTexture
 {
 public:
-	bool load(const std::string &filepath) { return ofLoadImage(tex_, filepath); }
+	bool load(const std::filesystem::path &filepath) override { return ofLoadImage(tex_, filepath); }
 	void setTexture(ofTexture tex) { tex_ = tex; }
-	float getWidth() const { return tex_.getWidth(); }
-	float getHeight() const { return tex_.getHeight(); }
-	void draw(float x, float y, float w, float h) const {
+	float getWidth() const override { return tex_.getWidth(); }
+	float getHeight() const override { return tex_.getHeight(); }
+	void draw(float x, float y, float w, float h) const override {
 		tex_.draw(x,y,w,h);
 	}
+	ofTexture & getTexture() override { return tex_; }
+	const ofTexture & getTexture() const override { return tex_; }
+	void setUseTexture(bool bUseTex) override {}
+	bool isUsingTexture() const override { return true; }
 protected:
 	ofTexture tex_;
 };
